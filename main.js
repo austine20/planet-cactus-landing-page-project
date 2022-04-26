67983e7fb7e2052b7ee1fa6ade12b71551af77b5
Augustine Okoye <augustine.okoye@omnibiz.com>
2022-04-26 03:18:00 +0100

    first design commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// const scrollToBottomPage = document.querySelector("#scroll");
+
+// scrollToBottomPage.addEventListener("click", () => {
+//   console.log("clicked >>>>"windows.scrollTo(0, document.body.scrollHeight));
+// });
+
+// scroll fadeIn animation
+
+const isVisible = (el) => {
+  let elementBox = el.getBoundingClientRect();
+  let distanceFromTop = -200;
+
+  if (elementBox.top - window.innerHeight < distanceFromTop) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const scanDocument = () => {
+  let sectionList = document.querySelectorAll(".hidden");
+  sectionList.forEach((section) => {
+    if (isVisible(section)) {
+      section.classList.remove("hidden");
+    }
+  });
+};
+
+document.addEventListener("scroll", scanDocument);
+
+// scroll to bottom functionality
+
+const bgToggle = document.querySelector("#toggle");
+
+bgToggle.addEventListener("change", () => {
+  document.body.classList.toggle("light");
+});
